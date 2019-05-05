eda836c8c79d763d23da2092c534c4b42f79bb03
helix48 <sethjhammell@gmail.com>
2019-05-04 22:38:37 -0400

    fixed timers

--- index.js
+++ index.js
@@ -10,15 +10,26 @@ var games = [];
 var times = [];
 var moves = [];
 
+function timer() {
+  for (var i = 0; i < moves.length; i++) {
+    if (moves[i] == 1) {
+      times[i] += 1;
+    }
+  }
+}
+
 io.on('connection', function(socket){
   games.push(socket.id);
   var idnum = games.indexOf(socket.id);
   times.push(0);
   moves.push(0);
+  if (moves.length == 2) {
+    setInterval(timer, 1000);
+  }
   io.emit('chat message', "Player " + (idnum + 1) + " has joined.");
   if (idnum % 2 == 1) {
     io.emit('chat message', "Game between Player " + idnum + " and Player " + (idnum + 1) + " has started.")
-    moves[idnum - 1] = new Date();
+    moves[idnum - 1] = 1;
   }
   //console.log(games);
   var num = games.indexOf(socket.id)
@@ -26,23 +37,22 @@ io.on('connection', function(socket){
   console.log(moves);
   socket.on('chat message', function(msg){
     if (moves[num] != 0) {
-      times[num] += ((new Date()) - moves[num]);
       moves[num] = 0;
       if (num % 2 == 0) {
-        moves[num + 1] = new Date();
-        io.to(`${games[num + 1]}`).emit("Your opponent has played");
+        moves[num + 1] = 1;
+        io.to(`${games[num + 1]}`).emit('chat message', "Your opponent has played " + msg);
       }
       else {
-        moves[num - 1] = new Date();
-        io.to(`${games[num - 1]}`).emit("Your opponent has played");
+        moves[num - 1] = 1;
+        io.to(`${games[num - 1]}`).emit('chat message', "Your opponent has played " + msg);
       }
     }
     console.log(games.indexOf(socket.id));
     var gamenum = "(Game: " + (Math.floor(games.indexOf(socket.id) / 2) + 1) + ") ";
-    var time = "(Time Used: " + (Math.floor(times[num] / 1000)) + ") ";
+    var time = "(Time Used: " + Math.floor(times[num]) + ") ";
     var mssg =  time + gamenum + "Player " + (games.indexOf(socket.id) + 1) + ": " + msg;
     io.emit('chat message', mssg);
-    io.to(socket.id).emit("test");
+    //io.to(socket.id).emit('chat message', 'test');
   });
 });
 
