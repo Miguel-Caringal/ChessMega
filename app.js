80e44b65d68573c36e49ae69fcb570f6b8222241
Miguel Caringal <Miguel.Caringal@gmail.com>
2019-05-04 22:39:03 -0400

    changes

--- app.js
+++ app.js
@@ -1,41 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+io.on('connection', function(socket){
+  console.log('a user connected');
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
-module.exports = app;
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
---
+++ server.js
@@ -0,0 +1,54 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+var games = [];
+var times = [];
+var moves = [];
+
+io.on('connection', function(socket){
+  games.push(socket.id);
+  var idnum = games.indexOf(socket.id);
+  times.push(0);
+  moves.push(0);
+  io.emit('chat message', "Player " + (idnum + 1) + " has joined.");
+  if (idnum % 2 == 1) {
+    io.emit('chat message', "Game between Player " + idnum + " and Player " + (idnum + 1) + " has started.")
+    moves[idnum - 1] = new Date();
+  }
+  //console.log(games);
+  var num = games.indexOf(socket.id)
+  console.log(times);
+  console.log(moves);
+
+
+  socket.on('chat message', function(msg){
+  	io.emit('gamenum', idnum);
+    if (moves[num] != 0) {
+      times[num] += ((new Date()) - moves[num]);
+      moves[num] = 0;
+      if (num % 2 == 0) {
+        moves[num + 1] = new Date();
+        io.to(`${games[num + 1]}`).emit("Your opponent has played");
+      }
+      else {
+        moves[num - 1] = new Date();
+        io.to(`${games[num - 1]}`).emit("Your opponent has played");
+      }
+    }
+    console.log(games.indexOf(socket.id));
+    var gamenum = "(Game: " + (Math.floor(games.indexOf(socket.id) / 2) + 1) + ") ";
+    var time = "(Time Used: " + (Math.floor(times[num] / 1000)) + ") ";
+    var mssg =  time + gamenum + "Player " + (games.indexOf(socket.id) + 1) + ": " + msg;
+    io.emit('chat message', mssg);
+    io.to(socket.id).emit('chat message',"test");
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
